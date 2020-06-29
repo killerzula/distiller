3dabe0b248095b469e1adc66129eb8d6d2f4bbc8
Shazib Summar Roy <ssummar.bee16seecs@seecs.edu.pk>
2020-06-29 18:25:24 +0500

    Update config.py

--- distiller/config.py
+++ distiller/config.py
@@ -146,7 +146,7 @@ def file_config(model, optimizer, filename, scheduler=None, resumed_epoch=None):
         msglogger.info('Reading compression schedule from: %s', filename)
         try:
             sched_dict = distiller.utils.yaml_ordered_load(stream)
-            distiller.utils.log_compression_schedule(path_ = filename, logger_ = msglogger)
+            #distiller.utils.log_compression_schedule(path_ = filename, logger_ = msglogger)
             return dict_config(model, optimizer, sched_dict, scheduler, resumed_epoch)
         except yaml.YAMLError as exc:
             print("\nFATAL parsing error while parsing the schedule configuration file %s" % filename)
